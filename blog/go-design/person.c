17c9b8dafed8751fa51d10a3ae7c612c660682ce
Alex Herrmann <alexandermherrmann@gmail.com>
2015-10-19 11:58:06 -0600

    Got person.c compiling with both clang 3.6 and gcc
    this is why C is unproductive :)

--- blog/go-design/person.c
+++ blog/go-design/person.c
@@ -1,18 +1,19 @@
 #include "stdio.h"
 #include "string.h"
+#include "stdlib.h"
 
 struct person {
   char* name;
-}
+};
 
-void say(struct person p, char* s) {
+int say(struct person p, char* s) {
   printf("%s says: %s\n", p.name, s);
+  return 0;
 }
 
 int main(int argc, char const *argv[]) {
   struct person Alex;
-  strcpy(Alex.name, "Alex");
-
-  say(Alex, "Hello World!");
-  return 0;
+  char* name = "Alex";
+  Alex.name = name;
+  return say(Alex, "Hello World!");
 }
